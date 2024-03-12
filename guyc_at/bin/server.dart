c7072bda09a57d3e882e21eb9250454b6e0c93fe
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-03-12 22:29:46 +0200

    htmx <3
    Former-commit-id: ae42f453eb379a998ad326c5a921ab7ab8b18db2

--- guyc_at/bin/server.dart
+++ guyc_at/bin/server.dart
@@ -4,29 +4,35 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
-
 Response _rootHandler(Request request) {
-  return Response.ok('Hello, world!\n');
+  return Response.ok('Hello, world!');
 }
 
 Response _echoHandler(Request request) {
   final message = request.params['message'];
-  return Response.ok('$message\n');
+  return Response.ok('$message');
 }
 
+Response _firstName(Request request) {
+  return Response.ok('Guy');
+}
+
+Response _lastName(Request request) {
+  return Response.ok('Chait');
+}
+
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler)
+  ..get('/name/first', _firstName)
+  ..get('/name/last', _lastName);
+
 void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
-  // Configure a pipeline that logs requests.
   final handler =
       Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
 
-  // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
